131b129e0da34966be12193ee952163329dfcb6a
iwata-jumpei-z <jumpei.iwata@zigexn.co.jp>
2025-06-06 10:58:41 +0900

    画像URLが入ったCSVを分割できるように変更

---
+++ split-images-csv/config.py
@@ -0,0 +1,58 @@
+CSV_FILE_PATH = 'split-images-csv/sample.csv'
+"""
+分割したいCSVファイルのパス
+"""
+
+OUTPUT_PATH = 'split-images-csv/output'
+"""
+分割したCSVファイルの出力先パス
+"""
+
+ALL_FOLDER_NAMES = {
+    1: 'plan',
+    2: 'exterior',
+    3: 'map', 
+    4: 'neighborhood',
+    5: 'living',
+    9: 'other',
+    10: 'doorway',
+    11: 'living',
+    12: 'kitchen',
+    13: 'living',
+    14: 'living',
+    15: 'bathroom',
+    None: 'unknown',
+}
+
+# ----- フォルダリストの振り分け ----- 
+
+FOLDER_NAME_LIST_1 = {
+    1: 'plan',
+    2: 'exterior',
+    3: 'map', 
+    4: 'neighborhood',
+    None: 'unknown',
+}
+"""
+分担１つ目のフォルダリスト
+"""
+
+FOLDER_NAME_LIST_2 = {
+    5: 'living',
+    11: 'living',
+    13: 'living',
+    14: 'living',
+}
+"""
+分担２つ目のフォルダリスト
+"""
+
+FOLDER_NAME_LIST_3 = {
+    9: 'other',
+    10: 'doorway',
+    12: 'kitchen',
+    15: 'bathroom',
+}
+"""
+分担３つ目のフォルダリスト
+"""
---
+++ split-images-csv/execute.py
@@ -0,0 +1,52 @@
+from config import CSV_FILE_PATH, OUTPUT_PATH, ALL_FOLDER_NAMES, FOLDER_NAME_LIST_1, FOLDER_NAME_LIST_2, FOLDER_NAME_LIST_3
+import csv
+import os
+
+def main():
+  if not (ALL_FOLDER_NAMES == {**FOLDER_NAME_LIST_1, **FOLDER_NAME_LIST_2, **FOLDER_NAME_LIST_3}):
+      raise ValueError("フォルダリストの分担に不足があります。振り分けミスがないか確認してください。")
+  divide_csv_file(
+      csv_file_path = CSV_FILE_PATH,
+      output_path = OUTPUT_PATH,
+      folder_name_list_1 = FOLDER_NAME_LIST_1,
+      folder_name_list_2 = FOLDER_NAME_LIST_2,
+      folder_name_list_3 = FOLDER_NAME_LIST_3
+  )
+
+
+# ----- modules -----
+
+def divide_csv_file(csv_file_path, output_path, folder_name_list_1, folder_name_list_2, folder_name_list_3):
+    os.makedirs(output_path, exist_ok=True)
+    output_files = {
+        "splited_images_1.csv": folder_name_list_1,
+        "splited_images_2.csv": folder_name_list_2,
+        "splited_images_3.csv": folder_name_list_3,
+    }
+    file_handlers = {filename: open(os.path.join(output_path, filename), mode='w', newline='', encoding='utf-8') for filename in output_files}
+    csv_writers = {filename: csv.writer(file) for filename, file in file_handlers.items()}
+
+    try:
+        with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            header = next(reader)
+            for writer in csv_writers.values():
+                writer.writerow(header)
+            for row in reader:
+                label = row[1]
+                if label.isdigit():
+                    label = int(label)
+                    for filename, folder_list in output_files.items():
+                        if label in folder_list:
+                            csv_writers[filename].writerow(row)
+                            break
+    finally:
+        # ファイルを閉じる
+        for file in file_handlers.values():
+            file.close()
+
+
+# ----- エントリーポイント -----
+
+if __name__ == "__main__":
+    main()
