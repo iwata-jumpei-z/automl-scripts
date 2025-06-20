5942ee6e606c931066550ff6a034cb04ba81445d
iwata-jumpei-z <jumpei.iwata@zigexn.co.jp>
2025-06-20 19:14:38 +0900

    リポジトリ整理

--- split-images-csv/bq-query.sql
+++ split-images-csv/bq-query.sql
@@ -1,33 +1,28 @@
+WITH ranked_images AS (
+  SELECT
+    t.global_key,
+    t.building_key,
+    img.type AS label,
+    img.thumbnail_url AS URL,
+    ROW_NUMBER() OVER (PARTITION BY img.type, t.building_key ORDER BY img.thumbnail_url, t.global_key) AS rn_building_key,
+    ROW_NUMBER() OVER (PARTITION BY img.type ORDER BY t.building_key, img.thumbnail_url, t.global_key) AS rn_total
+  FROM
+    `lake_site.smocca_solr_group_with_images` AS t,
+    UNNEST(t.images) AS img
+  WHERE
+    t.exported_at = "2025-06-09"
+    AND t.apaman_only_flag = True
+)
+
 SELECT
-    sub.global_key,
-    sub.label,
-    sub.URL,
-    sub.building_key
+  global_key,
+  label,
+  URL,
+  building_key,
 FROM
-    (
-        SELECT
-            t.global_key,
-            t.building_key,
-            img.type AS label,
-            img.thumbnail_url AS URL,
-            ROW_NUMBER() OVER (PARTITION BY img.type, t.building_key ORDER BY img.thumbnail_url, t.global_key) AS rn_building_key,
-            ROW_NUMBER() OVER (PARTITION BY img.type ORDER BY t.building_key, img.thumbnail_url, t.global_key) AS rn_total
-        FROM
-            `lake_site.smocca_solr_group_with_images` AS t,
-            UNNEST(t.images) AS img
-        WHERE
-            t.exported_at = "2025-06-01"
-            AND r_only_flag = true
-    ) AS sub
+  ranked_images
 WHERE
-    sub.rn_building_key = 1
-    AND (
-        CASE 
-            WHEN sub.label = '9' THEN sub.rn_total <= 3000   -- other
-            WHEN sub.label = '10' THEN sub.rn_total <= 2000  -- doorway
-            WHEN sub.label = '15' THEN sub.rn_total <= 2000  -- bathroom
-            ELSE sub.rn_total <= 1500   -- デフォルトは1500枚分のURLを収集
-        END
-    )
+  rn_building_key = 1
+  AND label in ("3", "5","9", "15")
 ORDER BY
-    sub.label, sub.building_key;
+  label, building_key;
